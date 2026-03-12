ebedfc834c6046c1d1115f131469273c602f4ef0
impactabdou <kabarabdou1@gmail.com>
2026-03-12 12:13:20 +0100

    feat: internationalisation

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <QApplication>
+#include <QTranslator>
+#include <QLocale>
 #include "view/MainWindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    QTranslator translator;
+
+    QString locale = QLocale::system().name();
+
+    if (translator.load("://mandala_" + locale + ".qm") ||
+        translator.load("://mandala_" + locale.left(2) + ".qm")) {
+        app.installTranslator(&translator);
+    }
 
     MainWindow window;
     window.show();
